09840375e62b8e850636a55cee429582ae9d96fb
aitahcen <aitahcene.sofia@hotmail.fr>
2021-02-20 11:21:24 +0100

    clustering coeff

--- TP3.java
+++ TP3.java
@@ -2,29 +2,22 @@
 public class TP3 {
 	
 	public static void main(String []args) {
-		//String file = args[1];
-		//int estimNbAretes = Integer.parseInt(args[2]);
+		String file = args[1];
+		int estimNbAretes = Integer.parseInt(args[2]);
 		
-		//ReadFile r = new ReadFile(estimNbAretes, file);
-		
-		ReadFile r = new ReadFile(11095298, "C:\\Users\\aitah\\Desktop\\MIDS\\S2\\GRI\\Graphes_TP2\\as-skitter-simple.txt");
+		ReadFile r = new ReadFile(estimNbAretes, file);
 		
 		Graph g = new Graph(r.nbSommets, r.nbAretes, r.sommets1, r.sommets2, r.compteur);
 		
-		/*if(args[0].equals("triangles")) {
-		 	int[] nbTriangles = new int[g.nbSommets];
-			Triangles t = new Triangles(g,1,nbTriangles);
-			//Triangles t = new Triangles(g,Integer.parseInt(args[3]),nbTriangles);
-			nbTriangles = t.triangles(1);
-			//System.out.println(nbTriangles[Integer.parseInt(args[3])]);
-			System.out.println(nbTriangles[1]);
-		}*/
+		if(args[0].equals("triangles")) {
+			Triangles t = new Triangles(g,Integer.parseInt(args[3]));
+			System.out.println(t.triangles());
+		}
 		
-		//if(args[0].equals("clust")) {
-			int[] nbTriangles = new int[g.nbSommets];
-			Triangles t = new Triangles(g,0,nbTriangles);
-			t.clust(g);
-		//}
+		if(args[0].equals("clust")) {
+			Triangles t = new Triangles(g,0);
+			t.clust();
+		}
 	}
 
 }
--- Triangles.java
+++ Triangles.java
@@ -4,36 +4,27 @@ public class Triangles {
 	public Graph g;
 	public boolean[] voisins;
 	public boolean[] visited;
-	public int[] nbTriangles;
-	//public double degre;
 	
-	public Triangles(Graph g, int u, int[] nbTriangles) {
+	public Triangles(Graph g, int u) {
 		this.sommet = u;
 		this.g = g;
-		this.nbTriangles = nbTriangles;
-		//this.degre = 0;
 		
 		this.voisins = new boolean[g.nbSommets];
 		this.visited = new boolean[g.nbSommets];
 		
 		for(int v: g.neighbors(this.sommet)) {
 			this.voisins[v] = true;
-			//this.degre++;
 		}
 	}
 	
-	public int[] triangles(int exo) {
-		//int nbTriangles = 0;
+	public int triangles() {
+		int nbTriangles = 0;
 		
-		//for(int v: g.neighbors(sommet)) {
-		
-		for(int i=0; i<voisins.length; i++) {
-			if(voisins[i] && (exo==1 || i>sommet) && !visited[i]) {
+		for(int i=0; i<voisins.length; i++) {	
+			if(voisins[i] && !visited[i]) {
 				for(int w: g.neighbors(i)) {
-					if((exo==1 || w>sommet) && voisins[w] && !visited[w]) {
-						this.nbTriangles[sommet]++;
-						this.nbTriangles[i]++;
-						this.nbTriangles[w]++;
+					if(voisins[w] && !visited[w]) {				
+						nbTriangles++;
 					}
 				}
 				visited[i] = true;
@@ -42,28 +33,31 @@ public class Triangles {
 		return nbTriangles;				
 	}
 	
-	public void clust(Graph g) {
-		int[] x = triangles(2);
-		double cluLx;									// clustering local d'un sommet
-		if(g.degre(sommet)==0.0 || g.degre(sommet)==1.0) cluLx = 0.0;
-		else cluLx = (2.0*x[0])/(g.degre(sommet)*(g.degre(sommet)-1.0));
+	public void clust() {
+		// pour le sommet 0
+		int triX = triangles();
+		double cluLx = 0.0;									// clustering local d'un sommet
+		if(g.degre(sommet)>1) cluLx = (2.0*triX)/(g.degre(sommet)*(g.degre(sommet)-1.0));
 		double sommeCluL = cluLx;
+		double triG = triX;
+		double nv = g.degre(sommet)*(g.degre(sommet)-1.0);
 		
-	//	System.out.println(degre);
-		//System.out.println(tri);
-//		System.out.format("%.5f\n", cluLx);
-//		System.out.format("%.5f\n", sommeCluL);
-		
-		for(int i=1; i<g.nbSommets; i++) {		// voir si c'est pas nbSommets-1
-			Triangles t = new Triangles(g,i,x);
-			int[] y = t.triangles(2);
-			if(t.g.degre(t.sommet)==0.0 || t.g.degre(t.sommet)==1.0) cluLx = 0.0;
-			else cluLx = (2.0*y[i])/(t.g.degre(t.sommet)*(t.g.degre(t.sommet)-1.0));	
+		// pour les sommets à partir de 1
+		for(int i=1; i<g.nbSommets; i++) {	
+			Triangles t = new Triangles(g,i);
+			triX = t.triangles();
+			
+			if(t.g.degre(t.sommet)>1) cluLx = (2.0*triX)/(t.g.degre(t.sommet)*(t.g.degre(t.sommet)-1.0));
+			else cluLx = 0;
+			
 			sommeCluL += cluLx;
-
-		}
-		
+			triG += triX;
+			nv += t.g.degre(t.sommet)*(t.g.degre(t.sommet)-1.0);
+		}		
 		double cluL = sommeCluL/g.nbSommets;
 		System.out.format("%.5f\n", cluL);
+		
+		double cluG = triG/(nv/2);
+		System.out.format("%.5f\n", cluG);
 	}
 }
